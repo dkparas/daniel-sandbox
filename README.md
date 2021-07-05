fffaad971ff99d66e25a386ea24d7ef5738165af
dkparas <daniel.k.paras@gmail.com>
2021-07-05 12:53:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-kpkcjt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kpkcjt)
---
+++ index.js
@@ -0,0 +1,34 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h5>Reference</h5>`;
+//coding//
+const Write = document.getElementById('tef');
+Write.innerHTML = `<h1>Hi, my name is Daniel</h1>`;
+//wow//
+const code = document.getElementById('rjc');
+code.innerHTML = `<h3>pomidory sa pomiedzy miesem i jajkami</h3>`;
+//wheeeee//
+const Reference = document.getElementById('hi_cow');
+Reference.innerHTML = `<h4>🚗 This is Stefan.</h4>`;
+//stefanyay
+const lin = document.getElementById('stefanwantcar');
+lin.innerHTML = `<h1>🚗 🚕 🚌 🚒 🛺 🚙. __ __ __ __ Stefan likes cars. __ _</h1>`;
+//martin//
+const sc = document.getElementById('martin');
+sc.innerHTML = '<h2>Martin🥥🍕</h2>';
+//eateateat//
+const comprizement = document.getElementById('yumyum');
+comprizement.innerHTML =
+  '<h2> 🌮🍕🧇🌭🥥🥗🥒 __ __ __ __ __ __ __ __ Martin likes food. __ __ __ __ __ </h2>';
+//veronica//
+const pretty = document.getElementById('flower');
+pretty.innerHTML = '<h5>❁ This is Veronica 😃</h5>';
+//dscr9r
+const fvlrx = document.getElementById('mlacrz');
+fvlrx.innerHTML = '<h2>💐🌸🌼🌹Veronica likes flowers.</h2>';
+//mememememememe//
+const manchester = document.getElementById('contre');
+manchester.innerHTML = '<h3>rgh</h3>';
